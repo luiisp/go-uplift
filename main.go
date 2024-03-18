5059dd4794cb4f7c8d7633015be344bcba0ddcd5
scr6w <playerzone06@gmail.com>
2024-03-18 20:57:32 -0300

    add error handler in rest_err

--- main.go
+++ main.go
@@ -1,16 +1,73 @@
-package main
+package rest_err
 
 import (
-	"log"
-
-	"github.com/joho/godotenv"
+	"net/http"
 )
 
-func main() {
+type RestErr struct { // custom errors -> json
+	Message string   `json:"message"`
+	Err     string   `json:"error"`
+	Code    int64    `json:"code"`
+	Causes  []Causes `json:"causes"`
+}
 
-	envs := godotenv.Load()
-	if envs == nil {
-		log.Fatal("Error in load envs")
+type Causes struct {
+	Field    string `json:"field"`
+	Messages string `json:"message"`
+}
+
+func (r *RestErr) Error() string {
+	return r.Message //all
+}
+
+// models errs
+
+func newRestErr(message, err string, code int64, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     err,
+		Causes:  causes,
+		Code:    code,
 	}
+}
 
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+	}
+}
+
+func NewBadRequestErrorValidationError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+}
+
+func newInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "internal_server_error",
+		Code:    http.StatusInternalServerError,
+	}
+}
+
+func NewNotFoundError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "not_found",
+		Code:    http.StatusNotFound,
+	}
+}
+
+func NewForbbidenError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "forbidden",
+		Code:    http.StatusForbidden,
+	}
 }
