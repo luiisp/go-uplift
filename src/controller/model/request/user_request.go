dbaefe9c0059d36134009c525ed2e49e9ade3eb6
scr6w <playerzone06@gmail.com>
2024-03-19 00:09:43 -0300

    add coments

--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -1,6 +1,6 @@
 package request
 
-type UserRequest struct {
+type UserRequest struct { // != gin -> validator
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,min=6,max=32,containsany=@#!$_"`
 	Nickname string `json:"nickname" binding:"min=1,max=32"`
